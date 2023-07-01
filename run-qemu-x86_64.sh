c36ba901bea87166919e49abaa8c24c6b79558d7
Razvan Deaconescu <razvand@unikraft.io>
2023-07-01 14:36:38 +0300

    Add Makefile, configuration files and scripts
    Update the README with instructions on setting up, configuring, building
    and running the Nginx app with Unikraft. As part of it, add Makefile
    files, configuration files and scripts; they are referenced in the
    `README.md` file.
    
    Current instructions cover the use of QEMU/KVM platform (both on x86_64 and
    on AArch64) and GCC. Firecraker/KVM, Xen, Linux platforms and Clang are
    not yet documented.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

---
+++ run-qemu-x86_64.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo ip link set dev virbr0 down 2> /dev/null
+sudo ip link del dev virbr0 2> /dev/null
+sudo ip link add dev virbr0 type bridge
+sudo ip address add 172.44.0.1/24 dev virbr0
+sudo ip link set dev virbr0 up
+
+sudo /usr/bin/qemu-system-x86_64 \
+    -fsdev local,id=myid,path=$(pwd)/fs0,security_model=none \
+    -device virtio-9p-pci,fsdev=myid,mount_tag=fs0,disable-modern=on,disable-legacy=off \
+    -netdev bridge,id=en0,br=virbr0 -device virtio-net-pci,netdev=en0 \
+    -append "netdev.ipv4_addr=172.44.0.2 netdev.ipv4_gw_addr=172.44.0.1 netdev.ipv4_subnet_mask=255.255.255.0 --" \
+    -kernel build/nginx_qemu-x86_64 \
+    -nographic
