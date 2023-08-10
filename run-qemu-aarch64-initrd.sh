69ec5e0db22fa842b52247e7aeebc5c043e0a0d0
Razvan Deaconescu <razvand@unikraft.io>
2023-08-10 17:53:55 +0300

    Add configuration files and run script for initrd
    Add support files for using initrd filesystem.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

---
+++ run-qemu-aarch64-initrd.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+kernel_image="build/nginx_qemu-arm64"
+
+if test $# -eq 1; then
+    kernel_image="$1"
+fi
+
+cd fs0
+find -depth -print | tac | bsdcpio -o --format newc > ../fs0.cpio
+cd ..
+
+sudo ip link set dev virbr0 down 2> /dev/null
+sudo ip link del dev virbr0 2> /dev/null
+sudo ip link add dev virbr0 type bridge
+sudo ip address add 172.44.0.1/24 dev virbr0
+sudo ip link set dev virbr0 up
+
+sudo qemu-system-aarch64 \
+    -kernel "$kernel_image" -nographic \
+    -netdev bridge,id=en0,br=virbr0 -device virtio-net-pci,netdev=en0 \
+    -append "netdev.ipv4_addr=172.44.0.2 netdev.ipv4_gw_addr=172.44.0.1 netdev.ipv4_subnet_mask=255.255.255.0 --" \
+    -initrd fs0.cpio \
+    -machine virt -cpu max
